45eca050122bea4dd84646355f8b48b3b076d5fc
chetas11 <cbirmole@gmail.com>
2020-11-01 23:04:40 +0530

    zomato searchAPI

---
+++ script.js
@@ -0,0 +1,55 @@
+sendGetRestaurant = async (query) => {
+    try {
+        const resp = await fetch("https://developers.zomato.com/api/v2.1/search?q="+query,{
+            headers: {"user-key": "33a4212611e9fef173ab4eb3a89775df"}
+          });
+        const data = await resp.json();
+        console.log(data)
+        for(let i=0; i<data.restaurants.length; i++){
+            CreateCard(data.restaurants[i].restaurant.thumb,data.restaurants[i].restaurant.name)
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+        let InputText;
+
+        let Searchbar = document.getElementById("search");
+        Searchbar.addEventListener("change", ()=>{
+            InputText = Searchbar.value
+        })
+
+        Searchbar.addEventListener("keyup", function(event) {
+            if (event.code === "Enter") {
+                event.preventDefault();
+                ResultRow.innerHTML = ""
+                sendGetRestaurant(InputText);
+            }
+        });
+
+
+let ResultRow = document.getElementById("results-row");
+
+
+function CreateCard(url,name){
+    let ResultCol = document.createElement("div");
+    ResultCol.classList.add("col-lg-4","col-md-4","col-sm-6")
+    let Card  = document.createElement("div");
+    Card.classList.add("card")
+    let img = document.createElement("img");
+    img.src = url;
+    img.alt = "restaurant-img"
+    let CardBody  = document.createElement("div");
+    CardBody.classList.add("card-body");
+    let Title = document.createElement("h5");
+    Title.innerText = name;
+    CardBody.appendChild(Title)
+    Card.appendChild(img)
+    Card.appendChild(CardBody)
+    ResultCol.appendChild(Card)
+    ResultRow.appendChild(ResultCol)
+}
+
+
+
